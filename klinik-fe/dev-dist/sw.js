a940bd63f221d9808d853e5cccd3c4afbc8b16b3
kahfi14-cmd <khildanashk14@gmail.com>
2026-06-15 19:50:03 +0700

    fix

--- klinik-fe/dev-dist/sw.js
+++ klinik-fe/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-3b23c8c3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.iou6puun3fk"
+    "revision": "0.ufst9h1invo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
