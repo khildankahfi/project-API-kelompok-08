d4997d77069dc986d7abd4b6a861ec059decd58e
kahfi14-cmd <khildanashk14@gmail.com>
2026-06-18 23:02:15 +0700

    add vercel origin to CORS

--- config/cors.php
+++ config/cors.php
@@ -19,8 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5500', 'https://cytoplast-mantis-unseated.ngrok-free.dev'],
-
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5500',
+        'https://cytoplast-mantis-unseated.ngrok-free.dev',
+        'https://project-api-kelompok-08.vercel.app',
+    ],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
